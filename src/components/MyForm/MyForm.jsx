51d93eef52c1f5e4bfe947901a26a21aafb1235e
Szymon1021 <105548800+Szymon1021@users.noreply.github.com>
2023-05-15 16:08:17 +0200

    krok3

--- src/components/MyForm/MyForm.jsx
+++ src/components/MyForm/MyForm.jsx
@@ -60,24 +60,27 @@ class MyForm extends Component {
         <h2>Phonebook </h2>
 
         <form className={styles.contact}>
-          <h4>Name</h4>
           <div>
+            <h4>Name</h4>
             <input
               type="text"
               name="name"
               pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
               required
+              placeholder="Name"
               onChange={this.handleChangeName}
             />
           </div>
           <div>
+            <h4>Number</h4>
             <input
               type="tel"
               name="number"
               pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
               title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
               required
+              placeholder="Number"
               onChange={this.handleChangeNumber}
             />
           </div>
